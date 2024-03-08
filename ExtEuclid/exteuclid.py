559b1348fdfa763ca1c9ac372d185e08797aa58c
Srujana203 <61263688+Srujana203@users.noreply.github.com>
2024-03-07 23:35:25 -0500

    sample values

--- ExtEuclid/exteuclid.py
+++ ExtEuclid/exteuclid.py
@@ -18,22 +18,29 @@ def multiplicative_inverse(a, m):
     else:
         return x % m
 
+print("GCD(10012012,2314213):", gcd(10012012, 2314213))
+print("GCD(28176412,29108188):", gcd(28176412, 29108188))
+print("GCD(38172,23812188):", gcd(38172, 23812188))
+print("Multiplicative inverse of 12091 mod 24123123:", multiplicative_inverse(12091, 24123123))
+print("Multiplicative inverse of 28173928 mod 129182771:", multiplicative_inverse(28173928, 129182771))
+print("GCD(381723029127918237717233210002,23812188332813212739187261):", gcd(381723029127918237717233210002, 23812188332813212739187261))
 
-operation = input("Choose operation:\n1. GCD\n2. Multiplicative inverse\nEnter operation number: ")
 
-if operation == '1':
-    # GCD operation
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
-    print("GCD({},{}) = {}".format(a, b, gcd(a, b)))
-elif operation == '2':
-    # Multiplicative inverse operation
-    a = int(input("Enter number: "))
-    m = int(input("Enter modulus: "))
-    try:
-        inverse = multiplicative_inverse(a, m)
-        print("Multiplicative inverse of {} mod {} = {}".format(a, m, inverse))
-    except ValueError as e:
-        print(e)
-else:
-    print("Invalid operation number")
+# operation = input("Choose operation:\n1. GCD\n2. Multiplicative inverse\nEnter operation number: ")
+
+# if operation == '1':
+#     # GCD operation
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     print("GCD({},{}) = {}".format(a, b, gcd(a, b)))
+# elif operation == '2':
+#     # Multiplicative inverse operation
+#     a = int(input("Enter number: "))
+#     m = int(input("Enter modulus: "))
+#     try:
+#         inverse = multiplicative_inverse(a, m)
+#         print("Multiplicative inverse of {} mod {} = {}".format(a, m, inverse))
+#     except ValueError as e:
+#         print(e)
+# else:
+#     print("Invalid operation number")
